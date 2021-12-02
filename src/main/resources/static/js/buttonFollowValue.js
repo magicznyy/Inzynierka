b5a684f17a6c31c3b7abcb7e53e071a0492f1f20
magicznyy <49494197+magicznyy@users.noreply.github.com>
2021-12-02 19:52:27 +0100

    Zmapowana tabela obserwowany uzytkownik

---
+++ src/main/resources/static/js/buttonFollowValue.js
@@ -0,0 +1,9 @@
+function change()
+{
+    var elem = document.getElementById("settings");
+    if (elem.value=="Obserwuj") elem.value = "Przestań obserwować";
+    else elem.value = "Obserwuj";
+
+
+}
+
